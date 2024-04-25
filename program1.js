2a28e5b8aca5ddd4aef8bc2acb31a7a0cbb40b9d
KartikChelawat <chelawatkartik@gmail.com>
2024-04-25 14:11:18 +0530

    Assessment update

--- program1.js
+++ program1.js
@@ -1,5 +1,25 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+    let max=0;
+    let l=0;
+    let r=0;
+    const st=new Set();
+    while(r<s.length)
+    {
+        if(!st.has(s.charAt(r))){
+            st.add(s.charAt(r));
+            max=Math.max(max,r-l+1);
+            r++;
+        }
+        else{
+            st.delete(s.charAt(l))
+            
+                l++;
+            
+        }
+        return max;
+    }
+
 }
 
 module.exports = { longestSubstring };
