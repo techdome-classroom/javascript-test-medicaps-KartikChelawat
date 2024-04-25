7f8ea7c386829fa4ace966083b07c5f2fe89df40
Kartik_Chelawat <135652547+KartikChelawat@users.noreply.github.com>
2024-04-25 14:17:36 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,33 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+  const n=arr.length;
+  let j=0;
+  for(let i=0;i<n;i++)
+  {
+    if(nums[i]<=0)
+    {
+      [nums[i],nums[j]]=[arr[j],arr[i]];
+      j++;
+    }
+  }
+  for(let i=0;i<n-j;i++)
+  {
+    const c=Math.abs(nums[i])-1+j;
+    if(c<n && nums[c]>0)
+    {
+      nums[c]=-nums[c];
+    }
+  }
+  for(let i=0;i<n-j;i++)
+  {
+    if(arr[i]>0)
+    {
+      return i+1;
+    }
+  }
+  return n+1;
 
 }
 
-module.exports = smallestMissingPositiveInteger;
+module.exports = {smallestMissingPositiveInteger};
